fccc9ddd326880b7b24f2b47cb194f5f78094fc9
Nelson Gardner-Challis <41055112+NelsonG-C@users.noreply.github.com>
2025-09-04 15:59:48 +0100

    add get_accuracies_from_log util to utils file, add honeypot_policy

--- src/lasr_labs_2025_control_project/apps/apps.py
+++ src/lasr_labs_2025_control_project/apps/apps.py
@@ -1,8 +1,3 @@
-from control_arena.eval.control_eval_def import get_control_task_product
-from control_arena.eval_mode import EvalMode
-from control_arena.suspicion_scorers import max_suspicion_scorer
-from inspect_ai import eval
-
 from lasr_labs_2025_control_project.apps.monitors.basic_monitor import (
     basic_monitor,
 )
@@ -19,9 +14,6 @@ from lasr_labs_2025_control_project.apps.prompts import (
     self_rec_attack_monitor_prompt,
     self_rec_attack_policy_prompt,
 )
-from lasr_labs_2025_control_project.apps.protocols.untrusted_monitoring_protocol import (
-    untrusted_monitoring_protocol,
-)
 from lasr_labs_2025_control_project.apps.utils.policy_gate import (
     policy_gate,
 )
@@ -30,6 +22,7 @@ from lasr_labs_2025_control_project.utils.setting import (
 )
 
 untrusted_model = "openai/gpt-4o"
+trusted_model = "openai/gpt-4o-mini"
 attack_policy_prompt = self_rec_attack_policy_prompt
 attack_monitor_prompt = self_rec_attack_monitor_prompt
 collusion_strategy_prompt = monitor_self_rec_collusion_strategy
@@ -53,6 +46,16 @@ untrusted_policy_builder = policy_gate(
     ),
 )
 
+honeypot_policy_builder = policy_gate(
+    honest=honest_policy(model=trusted_model, prompt=honest_policy_prompt),
+    attack=attack_policy(
+        model=trusted_model,
+        prompt=attack_policy_prompt,
+        collusion_strategy=collusion_strategy_prompt,
+        ephemeral_reminder=False,
+    ),
+)
+
 untrusted_monitor_builder = policy_gate(
     honest=basic_monitor(model=untrusted_model, prompt=honest_monitor_prompt),
     attack=basic_monitor(
@@ -61,15 +64,3 @@ untrusted_monitor_builder = policy_gate(
         collusion_strategy=collusion_strategy_prompt,
     ),
 )
-
-apps_task = get_control_task_product(
-    settings=apps_setting,
-    protocols=untrusted_monitoring_protocol(
-        policy_builder=untrusted_policy_builder,
-        monitor_builder=untrusted_monitor_builder,
-    ),
-    modes=[EvalMode.ATTACK],
-    monitoring_scorers=[max_suspicion_scorer()],
-)
-
-eval = eval(tasks=apps_task, limit=1)
